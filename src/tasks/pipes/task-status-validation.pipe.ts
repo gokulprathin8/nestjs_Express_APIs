85a4be3cfcf4b7c617211f136e2f1eea42c8d6a0
gokul <gokulprathin8@gmail.com>
2020-08-31 04:01:10 +0530

    Added DTO Validations

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,27 @@
+import { PipeTransform, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import { TaskStatus } from '../tasks.model';
+import { stat } from 'fs';
+
+export class TaskStatusValidationPipe implements PipeTransform {
+    readonly allowedStatuses = [
+        TaskStatus.OPEN,
+        TaskStatus.IN_PROGRESS,
+        TaskStatus.DONE
+    ]
+
+
+    transform(value: any, metadata: ArgumentMetadata) {
+        value = value.toUpperCase();
+
+        if (!this.isStatusValid(value)) {
+            throw new BadRequestException(`"${value}" is an invalid status`);
+        }
+
+        return value;
+    }
+
+    private isStatusValid(status: any) {
+        const idx = this.allowedStatuses.indexOf(status);
+        return (idx !== -1)
+    }
+}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,13 +3,14 @@ import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './tasks.model';
 import { CreateTaskDTO } from './dto/create-task.dto';
 import { GetTasksFilterDTO } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) {}
 
     @Get()
-    getTasks(@Query() filterDto: GetTasksFilterDTO): Task[] {
+    getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDTO): Task[] {
         if (Object.keys(filterDto).length) {
             return this.tasksService.getTasksWithFilters(filterDto);
         } else {
@@ -39,7 +40,7 @@ export class TasksController {
     @Patch('/:id/status')
     updateTaskStatus(
         @Param('id') id:string,
-        @Body('status') status: TaskStatus
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus
     ): Task {
         return this.tasksService.updateTaskStatus(id, status)
     }
