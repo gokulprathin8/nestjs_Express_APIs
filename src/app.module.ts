9b1eaec8763f82bc408c904f5ffdbae99f0bd8e1
Gokul Prathin <gokulprathin8@gmail.com>
2020-09-23 21:29:16 +0530

    Added DTO and JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
+import { ServiceController } from './service/service.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
-    TasksModule
-  ]
+    TasksModule,
+    AuthModule
+  ],
+  controllers: [ServiceController]
 })
 export class AppModule {}
---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,14 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
+export class AuthCredentialsDto {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(30)
+    username:string;
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(20)
+    @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {message: 'password must have 1 capital character, 1 small character and 1 special character'})
+    password:string;
+}
